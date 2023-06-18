e8c8e59114c9aeb6c7918f60e9f19f4cdac2a066
novalarama <novalakbarramadhany@gmail.com>
2023-06-18 15:29:57 +0700

    Finishing Responsive Profile Section

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -13,26 +13,13 @@ export default function Main () {
                         <span>Passionate to design and create it.</span>
                         <span>an enthusiastic UI/UX designer and front-end engineer specializing in website and mobile design. My passion lies in creating visually appealing interfaces that enhance user experiences. With a strong technical foundation, I seamlessly bring designs to life, ensuring both aesthetics and functionality.  My thrives in collaborative environments and is committed to delivering innovative and user-centric solutions.</span>
                     </div>
-                    <button className="button p-button">
+                    <button className="button p-button" onClick={() => window.open("https://www.linkedin.com/in/noval-akbar/", "_blank")}>
                         <span className="p-button-text">Connect with Me</span>
                     </button>
                 </div>
                 <div className="p-right">
-                    <img src="./assets/Ellipse.png"/>
-                    <img src="./assets/Photo.png"/>
-                    <img src="./assets/Title1.png"/>
-                    <img src="./assets/Title2.png"/>
-                    {/* <div style={{top: '-4%', left:'32%'}}>
-                        <FloatingDiv image={'./assets/Title1.png'} altText='Title 1'/>
-                    </div>
-                    <div style={{top: '-10rem', left: '18rem'}}>
-                        <FloatingDiv image={'./assets/Title2.png'} altText='Title 2'/>
-                    </div> */}
-                    <div className="p-board">
-                        <span>Hi, I'm Noval</span><br />
-                        <span>UI/UX Designer & Front-end Developer</span>
-                    </div>
-                </div>
+                <img src="./assets/Photo.png"/>
+                 </div>
             </div>
             </section>
         </div>
